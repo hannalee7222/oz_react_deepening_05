f2001ba5a1f26e2d9d049a94489e3add6b8726a4
hannalee7222 <hannah.lee7222@gmail.com>
2025-07-02 16:34:09 +0900

    🐛 코드 수정 및 오타 점검
    App.jsx -> ProductList.jsx -> Cart.jsx순으로 점검후 수정했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ const App = () => {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
+        if (cart.length > 8){
+            alert ('장바구니 최대수량은 8개 입니다.');
+            return;
+        }
+        setCart([...cart, product]);
+    }
         // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
         // 상품은 배열객체로 이루어져 있습니다.
         // 카트에 상품이 8개 이상이면 alert를 띄워
         // 더 이상 상품을 추가할 수 없다고 알려주세요.
-    };
 
     return (
         <>
